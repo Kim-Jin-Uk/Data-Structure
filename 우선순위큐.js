fccc43197decc03ecffe17da4c3298cfc901a1f4
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2022-07-29 14:45:02 +0900

    Study/ 자료구조/ 우선순위 큐 개선

--- 우선순위큐.js
+++ 우선순위큐.js
@@ -1,151 +1,69 @@
-class PriorityQueue {
-    constructor(){
-        class Node {
-            constructor(element){
-                this.data = element
-                this.parent = null
-                this.leftChild = null
-                this.rightChild = null
-                this.index = -1
-            }
-        }
-        let root = null
-        let current = null
-        let len = 0 
-        let printNode = []
-        this.length = function(){
-            return len
-        }
-        this.push = function(element){
-            const node = new Node(element)
-            node.index = len++
-            if(root === null) root = node
-            else{
-                while(current.length){
-                    const selectedNode = current.shift()
-                    if(selectedNode.leftChild === null){
-                        selectedNode.leftChild = node
-                        node.parent = selectedNode
-                        break
-                    }
-                    else if(selectedNode.rightChild === null){
-                        selectedNode.rightChild = node
-                        node.parent = selectedNode
-                        break
-                    }
-                    else{
-                        if(selectedNode.leftChild) current.push(selectedNode.leftChild)
-                        if(selectedNode.rightChild)current.push(selectedNode.rightChild)
-                    }
-                }
-            }
-            const heapQue = [node]
-            while(heapQue.length){
-                const heap = heapQue.shift()
-                const parent = heap.parent
-                if(heap && heap.parent){
-                    if(heap.data > parent.data){
-                        const tmp = heap.data
-                        heap.data = parent.data
-                        parent.data = tmp
-                        heapQue.push(parent)
-                    } 
-                }
-            }
-            current = [root]
-        }
-        this.pop = function(){
-            const result = root.data
-            root.data = - Number.MAX_VALUE
-            const elements = [root]
-            while(elements.length){
-                this.printIn()
-                const t = elements.shift()
-                if(t.leftChild && t.rightChild){
-                    if(t.leftChild.data < t.rightChild.data){
-                        const tmp = t.data
-                        t.data = t.rightChild.data
-                        t.rightChild.data = tmp
-                        elements.push(t.rightChild)
-                    }else{
-                        const tmp = t.data
-                        t.data = t.leftChild.data
-                        t.leftChild.data = tmp
-                        elements.push(t.leftChild)
-                    }
-                }else if(t.leftChild){
-                    const tmp = t.data
-                    t.data = t.leftChild.data
-                    t.leftChild.data = tmp
-                    elements.push(t.leftChild)
-                }else if(t.rightChild){
-                    const tmp = t.data
-                    t.data = t.rightChild.data
-                    t.rightChild.data = tmp
-                    elements.push(t.rightChild)
-                }else{
-                    const parent = t.parent
-                    if(parent.leftChild && parent.leftChild.data === -Number.MAX_VALUE){
-                        parent.leftChild = null
-                    }else if(parent.rightChild && parent.rightChild.data === -Number.MAX_VALUE){
-                        parent.rightChild = null
-                    }
-                } 
-            }
-            
-            return result
-        }
-        this.printPre = function(){
-            if(current === null) return console.log('none node')
-            printNode = []
-            this.preorder(current[0])
-            console.log(printNode.join())
-            printNode = []
-        }
-        this.printIn = function(){
-            if(current === null) return console.log('none node')
-            printNode = []
-            this.inorder(current[0])
-            console.log(printNode.join())
-            printNode = []
-        }
-        this.printPost = function(){
-            if(current === null) return console.log('none node')
-            printNode = []
-            this.postorder(current[0])
-            console.log(printNode.join())
-            printNode = []
-        }
-        this.preorder = function(node){
-            if(node === null) return
-            printNode.push(node.data)
-            this.preorder(node.leftChild)
-            this.preorder(node.rightChild)
-        }
-        this.inorder = function(node){
-            if(node === null) return
-            this.inorder(node.leftChild)
-            printNode.push(node.data)
-            this.inorder (node.rightChild)
-        }
-        this.postorder = function(node){
-            if(node === null) return
-            this.postorder(node.leftChild)
-            this.postorder (node.rightChild)
-            printNode.push(node.data)
-        }
+class Heap {
+    constructor() {
+        this.heap = []
     }
-}
+    swap(a, b) {
+        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]]
+    }
+    size() {
+        return this.heap.length
+    }
+    push(value) {
+        this.heap.push(value)
+        let idx = this.heap.length-1
+        let parent = Math.floor((idx-1)/2)
 
-const priorityQueue = new PriorityQueue()
-priorityQueue.push(1)
-priorityQueue.push(2)
-priorityQueue.push(3)
-priorityQueue.push(4)
-priorityQueue.push(5)
-priorityQueue.push(6)
-priorityQueue.push(7)
-priorityQueue.push(8)
-priorityQueue.printIn() 
-const max = priorityQueue.pop()
-priorityQueue.printIn() 
+        while(this.heap[parent] < value) {
+            this.swap(parent, idx)
+            idx = parent
+            parent = Math.floor((idx-1)/2)
+        }
+        return this.print()
+    }
+
+    pop() {
+        const lastIdx = this.heap.length-1
+        let idx = 0
+        this.swap(0, lastIdx)
+        let value = this.heap.pop()
+
+        while(idx < lastIdx) {
+            let leftChildIdx = idx*2+1
+            let rightChildIdx = idx*2+2
+            if(leftChildIdx >= lastIdx) {
+                break
+            } else if(rightChildIdx >= lastIdx)  {
+                if(this.heap[idx] < this.heap[leftChildIdx]) {
+                    this.swap(idx, leftChildIdx)
+                    idx = leftChildIdx
+                } else {
+                    break
+                }
+            } else {
+                if(this.heap[leftChildIdx] > this.heap[idx] && this.heap[rightChildIdx] > this.heap[idx]) {
+                    if(this.heap[leftChildIdx] > this.heap[rightChildIdx]) {
+                        this.swap(idx, leftChildIdx)
+                        idx = leftChildIdx
+                    } else {
+                        this.swap(idx,rightChildIdx)
+                        idx = rightChildIdx
+                    }
+                } else if(this.heap[leftChildIdx] > this.heap[idx]) {
+                    this.swap(leftChildIdx, idx)
+                    idx = leftChildIdx
+                } else if(this.heap[rightChildIdx] > this.heap[idx]) {
+                    this.swap(rightChildIdx, idx)
+                    idx = rightChildIdx
+                } else {
+                    break
+                }
+            }
+        }
+        return value
+    }
+
+    print() {
+        console.log(this.heap)
+    }
+
+}
